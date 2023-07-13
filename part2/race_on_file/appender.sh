6d92c90d5c427814b6045a9474d2e8a2519c1068
dmitry.karpov@wartsila.com <dmitry.karpov@wartsila.com>
2023-07-13 02:01:24 +0200

    Create symlink to resolve race condition

--- part2/race_on_file/appender.sh
+++ part2/race_on_file/appender.sh
@@ -10,8 +10,13 @@ echo "0" >> $file_name
 
 while true
 do
+  while true ; do
+    ln -s $file_name $file_name.lock 2> /dev/null
+    [ $? -ne 0 ] || break
+  done
   val=`tail -1 $file_name`
   echo "$val"
   val=$((val+1))
   echo "$val" >> $file_name
+  rm $file_name.lock
 done
